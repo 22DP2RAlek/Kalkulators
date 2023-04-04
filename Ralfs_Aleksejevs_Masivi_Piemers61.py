f36fef49adc4c4c6791025be864ef617517e3a71
Ralfs Aleksejevs <ra00007@rvt.lv>
2023-04-04 11:54:48 +0300

    temperaturas 1. versija

--- Ralfs_Aleksejevs_Masivi_Piemers61.py
+++ Ralfs_Aleksejevs_Masivi_Piemers61.py
@@ -1,6 +1,10 @@
-temperatura = [15,20,26,23,27,25,28]   # Aizpildām masīvu ar dotajām temperatūrām
+#programma p61a.1 versija
+diena = ['pirmdiena','otrdiena','tresdiena','ceturtdiena','piektdiena','sestdiena','svetdiena']
+temperatura = [0] * 7
+for numurs in range(7):
+    temperatura[numurs] = int(input('Ievadiet ' + diena[numurs] + 's gaisa temperaturu '))
 dienu_skaits = 0
 for numurs in range(7):
     if temperatura[numurs] > 25:
         dienu_skaits = dienu_skaits + 1   # Dienu skaitīšana
-print('Karstāko dienu skaits bija', dienu_skaits)
+print('Nedēļas karstāko dienu skaits bija', dienu_skaits)
